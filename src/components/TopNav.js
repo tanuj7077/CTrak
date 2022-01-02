c6543af2663d7c41fb74c31ed4df6c51cd4d303b
tanuj7077 <piku.tanuj@gmail.com>
2022-01-02 18:02:56 +0530

    global data

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useGlobalContext } from "../context";
 import { IoSearch } from "react-icons/io5";
 import { IoMdLogOut } from "react-icons/io";
 import { AiOutlineLogin } from "react-icons/ai";
 import { FaListUl } from "react-icons/fa";
 import Asset2 from "../assets/logo/Asset3.svg";
+import axios from "axios";
 
 function TopNav() {
   const {
@@ -14,6 +15,19 @@ function TopNav() {
     isLoggedIn,
     logout,
   } = useGlobalContext();
+  const [globalData, setGlobalData] = useState();
+  const getGlobalData = () => {
+    axios
+      .get(`${process.env.REACT_APP_BASE_URL}/coin/globalData`)
+      .then((res) => {
+        if (res.data.status.error_code === 0) {
+          setGlobalData(res.data.data);
+        }
+      });
+  };
+  useEffect(() => {
+    getGlobalData();
+  }, []);
   return (
     <div className="topNav">
       <div className="topNav-content">
@@ -57,30 +71,43 @@ function TopNav() {
           )}
         </div>
       </div>
-      <div className="topNav-global">
-        <div className="topNav-global-slider">
-          <p className="globalItem">
-            <span className="globalItem-topic">Cryptos:</span>
-            <span className="globalItem-value">15864</span>
-          </p>
-          <p className="globalItem">
-            <span className="globalItem-topic">Exchanges:</span>
-            <span className="globalItem-value">447</span>
-          </p>
-          <p className="globalItem">
-            <span className="globalItem-topic">Market Cap:</span>
-            <span className="globalItem-value">$2,146,216,890,682</span>
-          </p>
-          <p className="globalItem">
-            <span className="globalItem-topic">24h Vol:</span>
-            <span className="globalItem-value">$89,361,246,307</span>
-          </p>
-          <p className="globalItem">
-            <span className="globalItem-topic">Dominance:</span>
-            <span className="globalItem-value">BTC: 40.7% ETH: 21.1%</span>
-          </p>
+      {globalData && (
+        <div className="topNav-global">
+          <div className="topNav-global-slider">
+            <p className="globalItem">
+              <span className="globalItem-topic">Active Cryptos:</span>
+              <span className="globalItem-value">
+                {globalData.active_cryptocurrencies}
+              </span>
+            </p>
+            <p className="globalItem">
+              <span className="globalItem-topic">Active Exchanges:</span>
+              <span className="globalItem-value">
+                {globalData.active_exchanges}
+              </span>
+            </p>
+            <p className="globalItem">
+              <span className="globalItem-topic">Market Cap:</span>
+              <span className="globalItem-value">
+                ${globalData.quote.USD.total_market_cap.toFixed(2)}
+              </span>
+            </p>
+            <p className="globalItem">
+              <span className="globalItem-topic">24h Vol:</span>
+              <span className="globalItem-value">
+                ${globalData.quote.USD.total_volume_24h.toFixed(2)}
+              </span>
+            </p>
+            <p className="globalItem">
+              <span className="globalItem-topic">Dominance:</span>
+              <span className="globalItem-value">
+                BTC: {globalData.btc_dominance.toFixed(2)}% ETH:{" "}
+                {globalData.eth_dominance.toFixed(2)}%
+              </span>
+            </p>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
